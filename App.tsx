2bcaa5eaac19c86ddbceecaee264bc9d80473811
Julian <tipler.julian@gmail.com>
2022-04-02 21:25:04 -0500

    .env creation with firebase stuff

--- App.tsx
+++ App.tsx
@@ -5,6 +5,19 @@ import { Provider as PaperProvider } from "react-native-paper";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
